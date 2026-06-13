cf392d53f745342f010e9260e1b6c7c379baabb5
Jose Luna <josemanuelmluna@gmail.com>
2026-06-13 22:48:01 +0800

    feat: created Point struct and used as snake body

--- classes/Player.h
+++ classes/Player.h
@@ -1,17 +1,34 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-struct Player {
-    Player(int startX, int startY, char s)
-        : x(startX), y(startY), symbol(s){}
+#include <list>
+#include "Point.h"
 
-    int x;
-    int y;
+struct Player {
+    Player(int x, int y, char s)
+        : symbol(s){
+            body.emplace_back(x,y);
+        }
+
+    std::list<Point> body;
     char symbol;
-    void move(int dx, int dy){
-        x += dx;
-        y += dy;
-    };
+
+    void move(int dx, int dy, bool grow) {
+        Point currentHead = body.front();
+        Point newHead = {currentHead.x + dx, currentHead.y + dy};
+        body.push_front(newHead);
+        if (!grow) body.pop_back();
+    }
+
+    void grow(int dx, int dy) {
+        Point currentHead = body.front();
+        Point newHead = {currentHead.x + dx, currentHead.y + dy};
+        body.push_front(newHead);
+    }
+
+    Point& getHead() { return body.front(); }
+
 };
+
 
 #endif
